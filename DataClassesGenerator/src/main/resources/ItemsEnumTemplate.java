03b9cb6721d8af51fbf9e74366c5dd9175cf2da8
jay <70743392+jaydevelopsshit@users.noreply.github.com>
2022-08-29 23:22:42 -0700

    Create Items enum and Data Classes Generator

---
+++ DataClassesGenerator/src/main/resources/ItemsEnumTemplate.java
@@ -0,0 +1,33 @@
+package net.guardiandev.pluto.data;
+
+public enum Items {
+;
+
+    Items(int id, String name, String internalName) {
+        this.id = id;
+        this.name = name;
+        this.internalName = internalName;
+        this.type = null;
+        this.rarity = 0;
+        this.blockId = -1;
+        this.blockSubId = 0;
+    }
+
+    Items(int id, String name, String internalName, String type, int rarity, int blockId, int blockSubId) {
+        this.id = id;
+        this.name = name;
+        this.internalName = internalName;
+        this.type = type;
+        this.rarity = rarity;
+        this.blockId = blockId;
+        this.blockSubId = blockSubId;
+    }
+
+    public final int id;
+    public final String name;
+    public final String internalName;
+    public final String type;
+    public final int rarity;
+    public final int blockId;
+    public final int blockSubId;
+}
